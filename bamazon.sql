4251498c75a3af94130e14ceda52c4debff09f0a
A Shamrock <53099864+ashamrock@users.noreply.github.com>
2019-11-02 09:51:34 -0400

    Add files via upload

--- bamazon.sql
+++ bamazon.sql
@@ -5,22 +5,23 @@ CREATE DATABASE bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NULL,
   department_name VARCHAR(50) NULL,
   price DECIMAL(10,2) NULL,
   stock_quantity INT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("red leather jacket", "clothing", 100, 1),
-("fuzzy bunny slippers", "footware", 20, 150),
+VALUES ("fuzzy bunny slippers", "footware", 20, 150),
+("red leather jacket", "clothing", 100, 1),
 ("pig food", "dry goods", 25, 17),
-("cup with ball on string game", "toys", 25, 17),
+("cup with ball on string game", "toys", 25, 12),
 ("whisky glass", "kitchenware", 10, 40),
 ("kitcken table", "home", 4000, 5),
 ("boat hose", "garden", 18.07, 50),
 ("grape juice", "food", 1, 170),
-("baby diapers", "baby", 15, 4000),
+("baby diapers", "baby", 15, 1),
 ("really cool cell phone that actually works", "electronics", 25, 3),
+("Those really cute and yummy baby carrots", "food", 1, 3000);
--- customer.js
+++ customer.js
@@ -14,34 +14,73 @@ connection.connect(function(err) {
   console.log("\n ------------------------------------")
   console.log("  Welcome to BAMAZON online bazaar!\n  You are customer #" + connection.threadId );
   console.log("------------------------------------\n ")
-  listItems() 
+  readProducts() 
 });
 
-var listItems = function() {
+function readProducts() {
 	var query = "Select * FROM products";
 	connection.query(query, function(err, res) {
 		if (err) throw err;
+		console.log("----------------------------------------------")
 		for (var i = 0; i < res.length; i++) {
-			console.log("ID: " + res[i].id + " || " +res[i].product_name + " || Price: " + res[i].price);
-    }
-    console.log("\n");
-    pick();
-	});
+			console.log("ID: " + res[i].item_id + " - " +res[i].product_name + " - Price: " + res[i].price + " - quantity: " + res[i].stock_quantity);
+	}
+	console.log("----------------------------------------------\n")
+    selection();
+});
 };
 
-function pick() {
-  inquirer
-    .prompt({
-      name: "customer_search",
-      type: "input",
-      message: "Enter the ID of the item you would like to buy",
-    })
-    .then(function(answer) {
-      var query = "SELECT * FROM id WHERE ?";
-      connection.query(query, { products: answer.id }, function(err, res) {
-        if (err) throw err;
-          console.log(res.product_name + res.department_name + res.price + res,stock_quantity);
-      });
-      qty();
-    });
+function selection() {
+	inquirer.prompt([
+		
+		{
+			type: "input",
+			name: "item_id",
+			message: "Enter the Item ID for the product you would like to purchase.",
+		},{
+			type: "input",
+			name: "quantity",
+			message: "How many would you like to buy?",
+		}
+
+	]).then(function(input) {
+
+		var item = input.item_id;
+		var quantity = input.quantity;
+		var queryStr = "SELECT * FROM products WHERE ?";
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+
+		if (data.length > 0) {
+
+			var productData = data[0];
+
+			if (quantity <= productData.stock_quantity) {
+
+				var updateQueryStr = "UPDATE products SET stock_quantity = " + (productData.stock_quantity - quantity) + " WHERE item_id = " + item;
+
+				connection.query(updateQueryStr, function() {
+
+					setTimeout(function () {
+						console.log("\n\n*a total of $" + productData.price * quantity + " will automatically be charged to your credit card*\n\n\n");
+					
+					}, 1500);
+
+					setTimeout(function () {
+						readProducts() 
+					}, 3500);
+
+				})
+
+			} else { 
+				console.log("\n------------------------------------")
+				console.log("*Not enough stock on, please try again*");
+				console.log("------------------------------------\n")	
+				readProducts()		
+		}
+
+	}
+	})
+})
 }
+
